29d81c7b8d0d5ec0e3b85d329b40de3e1a5f7452
Philip Yoo <philipyoo10@gmail.com>
2016-10-22 21:11:52 -0700

    Add render and main js files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var ipc = require('electron').ipcRenderer;
+
 var welcomeScreen = document.getElementById("welcomeScreen")
 var installScreen = document.getElementById("installScreen")
 var mainScreen = document.getElementById("mainScreen")
@@ -9,9 +11,24 @@ var destroyField = document.getElementById("destroyField")
 var addButton = document.getElementById("addButton")
 var removeButton = document.getElementById("removeButton")
 
+var createBucket = document.getElementById("createBucket");
+
 installButton.addEventListener("click", INSTALLFUNCTION);
 createButton.addEventListener("click", CREATEBUCKET);
 destroyButton.addEventListener("click", DESTROYBUCKET);
 addButton.addEventListener("click", ADDFILE);
 removeButton.addEventListener("click", REMOVEFILE);
 
+
+createBucket.addEventListener('click', function(){
+    ipc.once('actionReply', function(response){
+      console.log("what is this")
+      console.log(response)
+    })
+
+    var name = createField.value;
+
+    console.log(name);
+
+    ipc.send('invokeAction', name);
+});
